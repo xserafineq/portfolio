b1cf10e8ccd23e9aa5dcab0636b74d37ee069689
xserafineq <serafin24m@gmail.com>
2026-02-24 17:09:50 +0100

    dodanie efektow wizualnych

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -5,7 +5,7 @@ export default function Card({title}:{title:string}) {
                  src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjhE-TIneks3x_lUbEJW-BuYJr7XN_hdI56A&s"}>
             </img>
             <div className={"title w-full text-xl font-medium text-center text-[#EDEDED] p-3"}>{title.charAt(0).toUpperCase() + "" + title.substring(1,title.length)}</div>
-            <button className={"show mx-auto flex p-2 border-3 w-3/4 h-[40px] border-[#78df71] text-[#78df71] font-medium items-center justify-center rounded-lg"}>Sprawdź</button>
+            <button className={"show mx-auto flex p-2 border-3 w-3/4 h-[40px] border-[#78df71] text-[#78df71] font-medium items-center justify-center rounded-lg hover:bg-[#78df71] hover:text-[#EDEDED] ease-in-out duration-500"}>Sprawdź</button>
             <div className={"technologies flex w-full mt-4 p-2 gap-2 justify-end text-[#EDEDED]"}>
                 <i className="devicon-react-original-wordmark text-2xl"></i>
                 <i className="devicon-typescript-plain text-2xl"></i>
